80ea955baeb8c1b0a093f35a33a49a2b4f83da5b
debasish-mihup <79203117+debasish-mihup@users.noreply.github.com>
2021-06-01 19:37:16 +0530

    Update mediainfo.go

--- mediainfo.go
+++ mediainfo.go
@@ -11,38 +11,38 @@ import (
 var mediainfoBinary = flag.String("mediainfo-bin", "mediainfo", "the path to the mediainfo binary if it is not in the system $PATH")
 
 type mediainfo struct {
-	XMLName xml.Name `xml:"MediaInfo"`
-	Media    media     `xml:"media"`
-	FilePath string  `xml:"ref,attr"`
+	XMLName  xml.Name `xml:"MediaInfo"`
+	Media    media    `xml:"media"`
+	FilePath string   `xml:"ref,attr"`
 }
 
 type track struct {
-	XMLName                   xml.Name `xml:"track"`
-	Type                      string   `xml:"type,attr"`
-	FormatProfile            string   `xml:"Format_Profile"`
-	FileExtension            string   `xml:"FileExtension"`
-	ColorSpace               string   `xml:"ColorSpace"`
-	ChromaSubsampling        string   `xml:"ChromaSubsampling"`
-	EncodedApplication       string   `xml:"Encoded_Application"`
-	StreamSizeProportion string   `xml:"StreamSize_Proportion"`
-	Width                     []string `xml:"Width"`
-	Height                    []string `xml:"Height"`
-	Format                    []string `xml:"Format"`
-	Duration                  []string `xml:"Duration"`
-	BitRate                  []string `xml:"BitRate"`
-	BitDepth                 []string `xml:"BitDepth"`
-	ScanType                 []string `xml:"ScanType"`
-	FileSize                 []string `xml:"FileSize"`
-	FrameRate                []string `xml:"FrameRate"`
+	XMLName                 xml.Name `xml:"track"`
+	Type                    string   `xml:"type,attr"`
+	FormatProfile           string   `xml:"Format_Profile"`
+	FileExtension           string   `xml:"FileExtension"`
+	ColorSpace              string   `xml:"ColorSpace"`
+	ChromaSubsampling       string   `xml:"ChromaSubsampling"`
+	EncodedApplication      string   `xml:"Encoded_Application"`
+	StreamSizeProportion    string   `xml:"StreamSize_Proportion"`
+	Width                   []string `xml:"Width"`
+	Height                  []string `xml:"Height"`
+	Format                  []string `xml:"Format"`
+	Duration                []string `xml:"Duration"`
+	BitRate                 []string `xml:"BitRate"`
+	BitDepth                []string `xml:"BitDepth"`
+	ScanType                []string `xml:"ScanType"`
+	FileSize                []string `xml:"FileSize"`
+	FrameRate               []string `xml:"FrameRate"`
 	Channels                []string `xml:"Channels"`
-	StreamSize               []string `xml:"StreamSize"`
+	StreamSize              []string `xml:"StreamSize"`
 	BitRateMode             []string `xml:"BitRate_Mode"`
-	SamplingRate             []string `xml:"SamplingRate"`
+	SamplingRate            []string `xml:"SamplingRate"`
 	FrameRateMode           []string `xml:"FrameRate_Mode"`
 	OverallBitRate          []string `xml:"OverallBitRate"`
 	DisplayAspectRatio      []string `xml:"Display_aspect_ratio"`
-	OverallBitRateMode     []string `xml:"OverallBitRate_Mode"`
-	FormatSettingsCABAC    []string `xml:"Format_Settings_CABAC"`
+	OverallBitRateMode      []string `xml:"OverallBitRate_Mode"`
+	FormatSettingsCABAC     []string `xml:"Format_Settings_CABAC"`
 	FormatSettingsRefFrames []string `xml:"Format_Settings_RefFrames"`
 }
 
@@ -126,53 +126,107 @@ func GetMediaInfo(fname string) (MediaInfo, error) {
 	menu := menu{}
 
 	if !IsInstalled() {
-		return info, fmt.Errorf("Must install mediainfo")
+		return info, fmt.Errorf("must install mediainfo")
 	}
 	out, err := exec.Command(*mediainfoBinary, "--Output=XML", "-f", fname).Output()
 
 	if err != nil {
 		return info, err
 	}
-	
+
 	if err := xml.Unmarshal(out, &mInfo); err != nil {
 		return info, err
 	}
 
 	for _, v := range mInfo.Media.Tracks {
 		if v.Type == "General" {
-			general.Duration = v.Duration[0]
-			general.Format = v.Format[0]
-			general.File_size = v.FileSize[0]
+			if len(v.Duration) > 0 {
+				general.Duration = v.Duration[0]
+			}
+			if len(v.Format) > 0 {
+				general.Format = v.Format[0]
+			}
+			if len(v.FileSize) > 0 {
+				general.File_size = v.FileSize[0]
+			}
 			if len(v.OverallBitRateMode) > 0 {
 				general.Overall_bit_rate_mode = v.OverallBitRateMode[0]
 			}
-			general.Overall_bit_rate = v.OverallBitRate[0]
-			general.File_extension = v.FileExtension
-			general.Frame_rate = v.FrameRate[0]
-			general.Stream_size = v.StreamSize[0]
+			if len(v.OverallBitRate) > 0 {
+				general.Overall_bit_rate = v.OverallBitRate[0]
+			}
+			if len(v.FileExtension) > 0 {
+				general.File_extension = v.FileExtension
+			}
+			if len(v.FrameRate) > 0 {
+				general.Frame_rate = v.FrameRate[0]
+			}
+			if len(v.StreamSize) > 0 {
+				general.Stream_size = v.StreamSize[0]
+			}
 		} else if v.Type == "Video" {
-			video.Width = v.Width[0]
-			video.Height = v.Height[0]
-			video.Format = v.Format[0]
-			video.Bit_rate = v.BitRate[0]
-			video.Duration = v.Duration[0]
-			video.Bit_depth = v.BitDepth[0]
-			video.Scan_type = v.ScanType[0]
-			video.Frame_rate = v.FrameRate[0]
-			video.Format_profile = v.FormatProfile
-			video.Format_settings__CABAC = v.FormatSettingsCABAC[0]
-			video.Format_settings__ReFrames = v.FormatSettingsRefFrames[0]
+			if len(v.Width) > 0 {
+				video.Width = v.Width[0]
+			}
+			if len(v.Height) > 0 {
+				video.Height = v.Height[0]
+			}
+			if len(v.Format) > 0 {
+				video.Format = v.Format[0]
+			}
+			if len(v.BitRate) > 0 {
+				video.Bit_rate = v.BitRate[0]
+			}
+			if len(v.Duration) > 0 {
+				video.Duration = v.Duration[0]
+			}
+			if len(v.BitDepth) > 0 {
+				video.Bit_depth = v.BitDepth[0]
+			}
+			if len(v.ScanType) > 0 {
+				video.Scan_type = v.ScanType[0]
+			}
+			if len(v.FrameRate) > 0 {
+				video.Frame_rate = v.FrameRate[0]
+			}
+			if len(v.FormatProfile) > 0 {
+				video.Format_profile = v.FormatProfile
+			}
+			if len(v.FormatSettingsCABAC) > 0 {
+				video.Format_settings__CABAC = v.FormatSettingsCABAC[0]
+			}
+			if len(v.FormatSettingsRefFrames) > 0 {
+				video.Format_settings__ReFrames = v.FormatSettingsRefFrames[0]
+			}
 		} else if v.Type == "Audio" {
-			audio.Format = v.Format[0]
-			audio.Channel_s_ = v.Channels[0]
-			audio.Duration = v.Duration[0]
-			audio.Bit_rate = v.BitRate[0]
-			audio.Frame_rate = v.FrameRate[0]
-			audio.Sampling_rate = v.SamplingRate[0]
-			audio.Format_profile = v.FormatProfile
+			if len(v.Format) > 0 {
+				audio.Format = v.Format[0]
+			}
+			if len(v.Channels) > 0 {
+				audio.Channel_s_ = v.Channels[0]
+			}
+			if len(v.Duration) > 0 {
+				audio.Duration = v.Duration[0]
+			}
+			if len(v.BitRate) > 0 {
+				audio.Bit_rate = v.BitRate[0]
+			}
+			if len(v.FrameRate) > 0 {
+				audio.Frame_rate = v.FrameRate[0]
+			}
+			if len(v.SamplingRate) > 0 {
+				audio.Sampling_rate = v.SamplingRate[0]
+			}
+			if len(v.FormatProfile) > 0 {
+				audio.Format_profile = v.FormatProfile
+			}
 		} else if v.Type == "Menu" {
-			menu.Duration = v.Duration[0]
-			menu.Format = v.Format[0]
+			if len(v.Duration) > 0 {
+				menu.Duration = v.Duration[0]
+			}
+			if len(v.Format) > 0 {
+				menu.Format = v.Format[0]
+			}
 		}
 	}
 	info = MediaInfo{General: general, Video: video, Audio: audio, Menu: menu}
